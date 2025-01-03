36b2b0965c2080e2872bce3ae3712d9d264bd896
thedraculaisreal <jakejackson2546@gmail.com>
2025-01-03 22:08:49 +0000

    splitting up words into structs and array

--- src/main.c
+++ src/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+
+struct word {
+    int count;
+    char buffer[15];
+} typedef word;
 
 int main(void){
     char buffer[1024];
@@ -7,9 +14,31 @@ int main(void){
     stream = fopen("black.txt", "r");
     int count = fread(&buffer, sizeof(char), sizeof(buffer), stream);
     fclose(stream);
+    word *array_words;
+    array_words = (word*)malloc(count * sizeof(word));
+    int j = 0;
+    int word_count = 0;
 
-    for(int i = 0; i <= count; i++){
-	printf("%c, ", buffer[i]);
+    for (int i = 0; i <= count; i++) {
+        if (buffer[i] == ' ' || buffer[i] == '\n' || buffer[i] == '\0') {
+            if (j < i) {
+                int difference = i - j;
+                char word_buf[difference + 1];
+                strncpy(word_buf, &buffer[j], difference);
+                word_buf[difference] = '\0'; // null terminate word.
+		// copy everything but null terminator
+                strncpy(array_words[word_count].buffer, word_buf, sizeof(array_words[word_count].buffer) - 1);
+                array_words[word_count].buffer[sizeof(array_words[word_count].buffer)] = '\0'; // add null terminator to end
+                word_count++;
+		// have to always null terminate or rjust doesnt work idk why :C.
+            }
+            j = i + 1; // move to the start of the next word
+        }
+    }
+
+    printf("WORDCOUNT = %d\n", word_count);
+    for(int i = 0; i < word_count; i++){
+	printf("%s, ", array_words[i].buffer);
     }
     printf("number of chars: %d\n", count);
     return 0;
